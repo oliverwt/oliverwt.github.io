bb240259745f417bbfb1f57e7b4529320b1ad565
oliverwt <oncampbell1@my.waketech.edu>
2025-04-11 21:43:22 -0400

    Add files via upload

---
+++ Gradebook/gradebook.js
@@ -0,0 +1,17 @@
+//
+function fetchGradeDate() {
+    //
+    console.log("Fetching grade data...")
+}
+
+//
+function populateGradebook(data) {
+    //
+    console.log("Populating gradebook with data", data);
+}
+
+//
+//
+const gradeData = fetchGradeDate();
+populateGradebook(gradeData);
+//
